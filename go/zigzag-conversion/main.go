09359ac785f399d525ff5664326012019706e991
GeekchanskiY <astrovskiy.04@gmail.com>
2024-05-27 20:46:35 +0300

    zigzag solved

--- go/zigzag-conversion/main.go
+++ go/zigzag-conversion/main.go
@@ -6,7 +6,8 @@ import (
 
 // https://docs.google.com/spreadsheets/d/1KTbVslo-2oXSWEpUGoSCyXUjV8N4IXhQUR9ACiLIjS0/edit?usp=sharing
 
-func convert(s string, numRows int) string {
+// I almost found mathemathical dependency for this problem, but I got really tired on finding the dependencies....
+func convert_old(s string, numRows int) string {
 	var sb strings.Builder
 	if numRows == 1 {
 		return s
@@ -31,13 +32,12 @@ func convert(s string, numRows int) string {
 	var rf int // Reducion factor
 	for j := 1; j < numRows-1; j++ {
 		counter = -j
-		if even {
+		if even == true {
 
-			rf = numRows*2 - (4 * j)
-			for i := 0; i < len(s); i++ {
+			rf = numRows*2 - (4 * j) + 1
+			for i := 0; i < len(s)-1; i++ {
 				if counter == 0 {
 					sb.WriteString(string(s[i]))
-					println("a")
 				}
 				counter += 1
 				if counter > rf {
@@ -46,6 +46,16 @@ func convert(s string, numRows int) string {
 			}
 			even = false
 		} else {
+			rf = numRows*2 - (4 * j) + 2
+			for i := 0; i < len(s)-1; i++ {
+				if counter == 0 {
+					sb.WriteString(string(s[i]))
+				}
+				counter += 1
+				if counter > rf {
+					counter = 0
+				}
+			}
 			even = true
 		}
 	}
@@ -63,6 +73,39 @@ func convert(s string, numRows int) string {
 	}
 
 	return sb.String()
+}
+
+// Submitted solution
+func convert(s string, numRows int) string {
+	if numRows < 2 || len(s) < 2 {
+		return s
+	}
+
+	rows := make([]string, numRows)
+
+	goingDown := false
+	row := 0
+
+	for _, char := range s {
+		rows[row] += string(char)
+
+		if (row == 0) || (row == numRows-1) {
+			goingDown = !goingDown
+		}
+
+		if goingDown {
+			row++
+		} else {
+			row--
+		}
+	}
+
+	result := ""
+	for _, row := range rows {
+		result += row
+	}
+
+	return result
 }
 
 func main() {
