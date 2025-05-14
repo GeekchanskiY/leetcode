fda72fd52b8d26eed1acaa15e3f95d0a3dd3e10d
GeekchanskiY <astrovskiy.04@gmail.com>
2025-05-14 13:54:30 +0300

    median of two sorted arrays: added best solution

--- go/median-of-two-sorted-arrays/main.go
+++ go/median-of-two-sorted-arrays/main.go
@@ -4,6 +4,63 @@ import (
 	"fmt"
 )
 
+// // Best solution
+//
+// func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+// 	l := len(nums1) + len(nums2)
+// 	i1 := 0
+// 	i2 := 0
+// 	var now int
+// 	for i1+i2 < l/2 {
+// 		if i1 == len(nums1) {
+// 			return findN(nums2, i2, l/2-(i1+i2), l%2)
+// 		}
+// 		if i2 == len(nums2) {
+// 			return findN(nums1, i1, l/2-(i1+i2), l%2)
+// 		}
+// 		if nums1[i1] < nums2[i2] {
+// 			now = nums1[i1]
+// 			i1++
+// 		} else {
+// 			now = nums2[i2]
+// 			i2++
+// 		}
+// 	}
+// 	if l%2 != 0 {
+// 		if i1 == len(nums1) {
+// 			return float64(nums2[i2])
+// 		} else if i2 == len(nums2) {
+// 			return float64(nums1[i1])
+// 		}
+// 		if nums1[i1] < nums2[i2] {
+// 			return float64(nums1[i1])
+// 		} else {
+// 			return float64(nums2[i2])
+// 		}
+// 	}
+// 	var next int
+// 	if i1 == len(nums1) {
+// 		next = nums2[i2]
+// 		return float64(now+next) / 2
+// 	} else if i2 == len(nums2) {
+// 		next = nums1[i1]
+// 		return float64(now+next) / 2
+// 	}
+// 	if nums1[i1] < nums2[i2] {
+// 		next = nums1[i1]
+// 	} else {
+// 		next = nums2[i2]
+// 	}
+// 	return float64(now+next) / 2
+// }
+// func findN(nums []int, head int, n int, one int) float64 {
+// 	if one == 1 {
+// 		return float64(nums[head+n])
+// 	} else {
+// 		return float64(nums[head+n-1]+nums[head+n]) / 2
+// 	}
+// }
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	var mergedArr []int
 
