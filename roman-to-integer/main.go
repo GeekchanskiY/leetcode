569cc3c8d7a55819e125c57c782344e7a4889130
GeekchanskiY <astrovskiy.04@gmail.com>
2025-05-20 16:27:13 +0300

    refactor repo structure

---
+++ roman-to-integer/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+var symbolValue = map[string]int
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
+    "M": 1000,
+}
+
+
+	var l int = len(s)
+	var r string
+	recent_val := 0
+	var current_val int
+	res := 0
+	for i := 0; i < l; i++ {
+		r = string(rune(s[i]))
+		current_val = symbolValue[r]
+		if current_val > recent_val && recent_val != 0 {
+			res += (current_val - recent_val)
+			recent_val = 0
+		} else {
+			res += recent_val
+			recent_val = current_val
+		}
+
+	}
+	if recent_val != 0 {
+		res += recent_val
+	}
+	return res
+    return res
+}
+func main() {
+	fmt.Println(romanToInt("III"))
+	fmt.Println(romanToInt("XIV"))
+	fmt.Println(romanToInt("XXXIII"))
+    fmt.Println(romanToInt("XXXIII"))
+
+}
