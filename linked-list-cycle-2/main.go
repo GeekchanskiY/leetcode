35632bb9062ea8342ec0efe1606e00849aeee2fe
GeekchanskiY <astrovskiy.04@gmail.com>
2025-05-22 11:36:43 +0300

    linked-list-cycle-2 ready

--- linked-list-cycle-2/main.go
+++ linked-list-cycle-2/main.go
@@ -1,33 +1,88 @@
 package main
 
+import (
+	"fmt"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func detectCycle(head *ListNode) *ListNode {
-	if head == nil {
-		return nil
-	}
-	if head.Next == nil {
+	if head == nil || head.Next == nil {
 		return nil
 	}
 
-	slow, fast := head, head.Next
-	for {
-		if fast.Next == nil {
-			return nil
-		}
-		if fast.Next.Next == nil {
-			return nil
-		}
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
 		slow = slow.Next
 		fast = fast.Next.Next
 
-		if fast.Next == slow {
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+
 			return slow
 		}
 	}
 
 	return nil
+}
+
+func main() {
+	listNode1 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	listNode2 := &ListNode{
+		Val:  2,
+		Next: listNode1,
+	}
+	listNode3 := &ListNode{
+		Val:  3,
+		Next: listNode2,
+	}
+	listNode4 := &ListNode{
+		Val:  4,
+		Next: listNode3,
+	}
+	listNode5 := &ListNode{
+		Val:  5,
+		Next: listNode4,
+	}
+	listNode6 := &ListNode{
+		Val:  6,
+		Next: listNode5,
+	}
+	listNode7 := &ListNode{
+		Val:  7,
+		Next: listNode6,
+	}
+	listNode8 := &ListNode{
+		Val:  8,
+		Next: listNode7,
+	}
+	listNode9 := &ListNode{
+		Val:  9,
+		Next: listNode8,
+	}
+	listNode10 := &ListNode{
+		Val:  10,
+		Next: listNode9,
+	}
+	listNode1.Next = listNode6
+
+	fmt.Println(detectCycle(listNode10).Val)
+
+	listNodeSelfRef := &ListNode{
+		Val: 1,
+	}
+	listNodeSelfRef.Next = listNodeSelfRef
+	fmt.Println(detectCycle(listNodeSelfRef).Val)
+
 }
