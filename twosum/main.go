883dbf99c3af53a3c457cee8c0db29810d8ab4ae
GeekchanskiY <astrovskiy.04@gmail.com>
2025-05-29 14:56:58 +0300

    twoSum better implementation added

--- twosum/main.go
+++ twosum/main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+func twoSumHashMap(nums []int, target int) []int {
+	res := make([]int, 2)
+	diffMap := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		diff := target - nums[i]
+
+		if j, found := diffMap[diff]; found {
+			res[0] = j
+			res[1] = i
+
+			return res
+		} else {
+			diffMap[nums[i]] = i
+		}
+	}
+
+	return res
+}
+
 func twoSum(nums []int, target int) []int {
 	for i1, x := range nums {
 		for i2, y := range nums {
@@ -19,4 +39,5 @@ func main() {
 	nums := []int{2, 7, 11, 15}
 	target := 9
 	fmt.Println(twoSum(nums, target))
+	fmt.Println(twoSumHashMap(nums, target))
 }
